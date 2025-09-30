86b0f08f115acbeb335a81e07643e4b85dbf6e59
Lerockinson <colton.rastagh@aquinas-sta.org>
2025-09-30 15:09:31 -0400

    Create Motor.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/Motor.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.teleops;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+}
