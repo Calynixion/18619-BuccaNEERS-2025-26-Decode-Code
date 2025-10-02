fc9d062e8a32e3b27f64781480b641b466679494
Calynixion <alphacentauri812@gmail.com>
2025-10-01 22:13:13 -0400

    Added FieldCentric TeleOp option
    -added gyro to drive.java, along with basic get yaw and yaw reset methods
    -Created FieldCentricTeleop.java
    -Created FieldCentricDrive command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -4,9 +4,13 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.ImuOrientationOnRobot;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 //creates a class for drive systems. Is a subsystem;inherits from subsystem base class provided by ftclib
 public class Drive extends SubsystemBase {
@@ -14,11 +18,16 @@ public class Drive extends SubsystemBase {
     MecanumDrive drivetrain;
     MotorEx FrontLeftM,FrontRightM,BackLeftM, BackRightM;
     Telemetry telemetry;
+    IMU imu;
     double multiplier = 1;
     double direction = 1;
 
     //constructor method creates an object of this class
     public Drive(HardwareMap hwMap, Telemetry telemetry){
+        //get gyro
+        imu = hwMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.FORWARD, RevHubOrientationOnRobot.UsbFacingDirection.LEFT)));
+
         //gets motor ids from driver station and makes them java objects
         this.FrontLeftM = new MotorEx(hwMap, "FrontLeftM");
         this.FrontRightM = new MotorEx(hwMap, "FrontRightM");
@@ -43,5 +52,14 @@ public class Drive extends SubsystemBase {
     //basic drive method
     public void robotCentricDrive(double x, double y, double rx) {
         drivetrain.driveRobotCentric(x * multiplier * direction, y * multiplier * direction, rx * multiplier * direction);
+    }
+    public void fieldCentricDrive(double x, double y, double rx) {
+        drivetrain.driveFieldCentric(x * multiplier * direction, y * multiplier * direction,rx * multiplier * direction, getFieldYaw());
+    }
+    public void resetIMU(){
+        imu.resetYaw();
+    }
+    public double getFieldYaw() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/FieldTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/FieldTeleOp.java
@@ -2,16 +2,18 @@ package org.firstinspires.ftc.teamcode.teleops;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import org.firstinspires.ftc.teamcode.commands.RobotCentricDrive;
-import org.firstinspires.ftc.teamcode.subsystems.Drive;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.commands.FieldCentricDrive;
+import org.firstinspires.ftc.teamcode.commands.RobotCentricDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+
 //OpMode is actual code that is initialized and ran, calls commands which call methods of subsystems
-@TeleOp(name="DefaultTeleOp")
-public class DefaultTeleOp extends CommandOpMode {
+@TeleOp(name="FieldTeleOp")
+public class FieldTeleOp extends CommandOpMode {
     //initialize variables
     Drive drivetrain;
-    RobotCentricDrive r_drive;
+    FieldCentricDrive f_drive;
     GamepadEx controller1;
 
     //initialize function runs when init is pressed on the driver station with this teleop selected
@@ -20,9 +22,9 @@ public class DefaultTeleOp extends CommandOpMode {
         //assign variables to their respective objects
         controller1 = new GamepadEx(gamepad1);
         drivetrain = new Drive(hardwareMap, telemetry);
-        r_drive = new RobotCentricDrive(drivetrain,controller1::getLeftX,controller1::getLeftY,controller1::getRightX);
+        f_drive = new FieldCentricDrive(drivetrain,controller1::getLeftX,controller1::getLeftY,controller1::getRightX);
 
         //sets the drivetrain subsystem to run the robot centric command continuously
-        drivetrain.setDefaultCommand(r_drive);
+        drivetrain.setDefaultCommand(f_drive);
     }
 }
