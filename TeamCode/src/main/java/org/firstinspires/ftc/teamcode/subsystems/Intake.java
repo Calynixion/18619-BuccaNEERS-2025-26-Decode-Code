daf45c92109a50a5f27812e11b2b1796a482dc9c
Calynixion <alphacentauri812@gmail.com>
2025-10-04 14:35:12 -0400

    Initial Commit of CRServo Intake
    -Created Intake.java, consisting of a motor assigned to a CRServo "IntakeS"
    -Basic spin and stop methods for Intake class
    -Instantiate Intake class in DefaultTeleOP
    -assign instant commands to spin and stop with A button on controller 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoController;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Intake extends SubsystemBase {
+    Motor IntakeS;
+    Telemetry telemetry;
+    double power = 1;
+    public Intake(HardwareMap hwMap, Telemetry telemetry){
+        this.IntakeS = new CRServo(hwMap,"IntakeS");
+        this.telemetry = telemetry;
+
+        IntakeS.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void spin(){
+        IntakeS.set(power);
+    }
+    public void stop(){
+        IntakeS.set(0);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/DefaultTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/DefaultTeleOp.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.teleops;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.Subsystem;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import org.firstinspires.ftc.teamcode.commands.RobotCentricDrive;
 import org.firstinspires.ftc.teamcode.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 //OpMode is actual code that is initialized and ran, calls commands which call methods of subsystems
@@ -13,6 +18,7 @@ public class DefaultTeleOp extends CommandOpMode {
     Drive drivetrain;
     RobotCentricDrive r_drive;
     GamepadEx controller1;
+    Intake intake;
 
     //initialize function runs when init is pressed on the driver station with this teleop selected
     @Override
@@ -21,8 +27,17 @@ public class DefaultTeleOp extends CommandOpMode {
         controller1 = new GamepadEx(gamepad1);
         drivetrain = new Drive(hardwareMap, telemetry);
         r_drive = new RobotCentricDrive(drivetrain,controller1::getLeftX,controller1::getLeftY,controller1::getRightX);
+        intake = new Intake(hardwareMap, telemetry);
 
         //sets the drivetrain subsystem to run the robot centric command continuously
         drivetrain.setDefaultCommand(r_drive);
+
+        controller1.getGamepadButton(GamepadKeys.Button.A)
+                .whileHeld(new InstantCommand(intake::spin))
+                .whenReleased(new InstantCommand(intake::stop));
+
+        register(drivetrain,intake);
     }
+
+
 }
