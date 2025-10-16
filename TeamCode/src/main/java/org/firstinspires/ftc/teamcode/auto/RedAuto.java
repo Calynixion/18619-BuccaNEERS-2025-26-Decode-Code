0bebe7e65a256d4a23807a0f6cde6c508f923d1c
MegaAwsomeReallyKoolThomas <mark.tarkhov1@gmail.com>
2025-10-16 15:23:35 -0400

    Added Semicolons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
@@ -2,9 +2,12 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+
 @Autonomous(name="RedAuto")
 public class RedAuto extends LinearOpMode {
-    Drive drivetrain
+    Drive drivetrain;
     @Override
     public void runOpMode() throws InterruptedException {
 
