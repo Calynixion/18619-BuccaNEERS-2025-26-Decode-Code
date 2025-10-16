4e81537b8c9abf6114698b93665bd64a937098c4
Calynixion <alphacentauri812@gmail.com>
2025-10-16 15:20:23 -0400

    Create RedAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+@Autonomous(name="RedAuto")
+public class RedAuto extends LinearOpMode {
+    Drive drivetrain
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
