8604874816a65191919a7ae8ba2c042dfec7cebd
MegaAwsomeReallyKoolThomas <mark.tarkhov1@gmail.com>
2025-10-23 15:30:22 -0400

    Camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AprilTagDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AprilTagDetectionTest.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline;
+import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline2;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -17,20 +18,22 @@ import java.util.ArrayList;
 
 @Autonomous
 public class AprilTagDetectionTest extends LinearOpMode {
-    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    AprilTagDetectionPipeline2 aprilTagDetectionPipeline;
     Drive drivetrain;
+    /*
     double fx = 679.2888908044871;
     double fy = 679.0590608430991;
     double cx = 399.04720194230583;
     double cy = 301.4138740002473;
     double tagsize = 0.173;
+     */
     @Override
     public void runOpMode() throws InterruptedException {
         //drivetrain = new Drive(hardwareMap, telemetry);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         OpenCvWebcam camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline2(telemetry);
         aprilTagDetectionPipeline.setDecimation(3);
         camera.setPipeline(aprilTagDetectionPipeline);
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
